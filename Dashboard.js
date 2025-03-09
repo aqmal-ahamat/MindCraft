30b3c68f67492962b6c14f4912be346009a72b9f
aqmal ahamat <ahamataqmal@gmail.com>
2025-03-09 18:56:29 +0530

    more dashboard styles

--- Dashboard.js
+++ Dashboard.js
@@ -1,11 +1,13 @@
 let username = localStorage.getItem("data")
-localStorage.clear();
 document.getElementById("usernametxt").textContent = username;
 
-
+//---------------------------------------------------------------------------Animations-----------------------------------------------------------------------------------------
 let introElement = document.getElementById("intro");
 
 introElement.addEventListener("animationend",()=>{
     introElement.style.display = "none";
     document.getElementById("content").style.display = "block";
 })
+
+
+// TODO  - clear the local storage only if the user is navigating to a different page
